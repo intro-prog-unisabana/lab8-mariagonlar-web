a16ad87185a1775218083528234c2113103e0968
María Fernanda Gonzalez Lara <mariagonlar@unisabana.edu.co>
2026-04-27 20:25:59 -0500

    problema 2

--- question1.py
+++ question1.py
@@ -25,3 +25,108 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+file_name = "tasks.txt"
+
+def load_tasks():
+    tasks = []
+    try:
+        file = open(file_name, "r")
+        lines = file.readlines()
+        file.close()
+
+        for line in lines:
+            tasks.append(line.strip())
+
+    except FileNotFoundError:
+        tasks = []
+
+    return tasks
+
+def save_tasks(tasks):
+    file = open(file_name, "w")
+
+    for task in tasks:
+        file.write(task + "\n")
+
+    file.close()
+
+def view_tasks():
+    tasks = load_tasks()
+
+    if tasks == []:
+        print("No tasks found.")
+        return
+
+    print("Your Tasks:")
+
+    i = 1
+    for task in tasks:
+        print(str(i) + ". " + task)
+        i = i + 1
+
+def add_task(task):
+    tasks = load_tasks()
+
+    tasks.append(task)
+
+    save_tasks(tasks)
+
+    print("Task added successfully.")
+
+def remove_task(number):
+    tasks = load_tasks()
+
+    try:
+        index = int(number)
+
+        if index < 1 or index > len(tasks):
+            print("Error: Invalid task number.")
+            return
+
+        removed_task = tasks.pop(index - 1)
+
+        save_tasks(tasks)
+
+        print("Removed task: " + removed_task)
+
+    except ValueError:
+        print("Error: Invalid input! Enter a valid task number.")
+
+def main():
+
+    if len(sys.argv) < 2:
+        print("Usage:")
+        print("python question1.py view")
+        print("python question1.py add \"Task name\"")
+        print("python question1.py remove <task_number>")
+        return
+
+    command = sys.argv[1]
+
+    if command == "view":
+        view_tasks()
+
+    elif command == "add":
+
+        if len(sys.argv) < 3:
+            print("Error: Missing task description.")
+            return
+
+        task = sys.argv[2]
+
+        add_task(task)
+
+    elif command == "remove":
+
+        if len(sys.argv) < 3:
+            print("Error: Missing task number.")
+            return
+
+        remove_task(sys.argv[2])
+
+    else:
+        print("Error: Unknown command.")
+
+
+main()
